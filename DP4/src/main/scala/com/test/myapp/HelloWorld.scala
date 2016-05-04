0e8db1928439d390db65fd1e498930091c4b0775
hduser <hduser@vkp>
2016-05-04 11:26:16 +0530

    next commit

---
+++ DP4/src/main/scala/com/test/myapp/HelloWorld.scala
@@ -0,0 +1,9 @@
+package com.test.myapp
+
+object HelloWorld {
+   def main(args: Array[String]) = {
+  println("Hello World!!");
+   val a = { val i = 10; val j = 100; j - i}
+   System.out.println(a);
+   }
+}
--- DP4/src/main/scala/com/test/myapp/WebFileProcessing.scala
+++ DP4/src/main/scala/com/test/myapp/WebFileProcessing.scala
@@ -3,24 +3,21 @@ package com.test.myapp
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
-object Wordcount {
+object WebFileProcessing {
   def main(args: Array[String]) = {
     val conf = new SparkConf()
-    .setAppName("WordCount")
+    .setAppName("WebFileProcessing")
     .setMaster("local")
     val sc = new SparkContext(conf)
     val test = sc.textFile("web_log.csv")    
     /*The test variable is a reference to a Resilient Distributed Dataset, abbreviated to RDD, which is the central abstraction in Spark: a read-only collection of objects that is
 partitioned across multiple machines in a cluster.*/
     
-    test.flatMap { line => 
-      line.split(",") 
-      }
-    .map {
-      word =>
-        (word,1)
-    }
-    .reduceByKey(_ + _)
+    val records = test.map(_.split(","))
+    .filter(rec => (rec(1) != "10.10.3.65" && rec(4).matches("[10737418240]")))
+    .map(rec => (rec(0).toInt, rec(1).toInt))
+    .reduceByKey((a, b) => Math.max(a, b))
+   
     .saveAsTextFile("output")
   }  
 }
